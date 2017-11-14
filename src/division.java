55c3c43d0d7d67b8a132c80c22fb5dc3414af251
juan acosta <juangabrielacosta@outlook.com>
2017-11-14 16:07:50 -0300

    edicion de division

--- src/division.java
+++ src/division.java
@@ -5,6 +5,8 @@ public class Division extends Operacion{
     public Division(double n1, double n2) {
              
         super(n1, n2, '/');
+		if (n2==0)System.out.println("no se puede dividir por cero");
+		else 
         this.div = n1 / n2;
         this.setRes(this.div);
     }    
