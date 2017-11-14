ab1658fdc6b61ea541754d64f68661a516a3f007
juan acosta <juangabrielacosta@outlook.com>
2017-11-14 12:08:00 -0300

    "clase Operacion"

---
+++ src/Operaciones.java
@@ -0,0 +1,20 @@
+
+public  class   Operaciones {
+	 double n1;
+	    double n2;
+	    double res;
+	    char op;
+	public Operaciones(double n1, double n2, char operacion) {
+		this.n1 = n1;
+        this.n2 = n2;
+        this.op = operacion;
+	}
+        public void mostrarResultado(){
+            
+            System.out.println(this.n1+" "+this.op+" "+this.n2+" = "+this.res);
+            
+        }
+		
+	}
+
+
