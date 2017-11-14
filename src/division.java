f1d85255d9db4cf031fd6a27d041965aa2a8a2c3
markososa23 <markososa23@gmail.com>
2017-11-14 14:43:11 -0300

    Creamo la funcion division

---
+++ src/division.java
@@ -0,0 +1,11 @@
+public class Division extends Operacion{
+    
+    double div = 0;
+       
+    public Division(double n1, double n2) {
+             
+        super(n1, n2, '/');
+        this.div = n1 / n2;
+        this.setRes(this.div);
+    }    
+}
